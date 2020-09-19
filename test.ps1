8b08ac0e9daab34889fb8a10453bd98eba148dbd
Adam Rush <adamrushuk@gmail.com>
2020-09-19 16:31:45 +0100

    Added improved comments and output

--- test.ps1
+++ test.ps1
@@ -4,28 +4,33 @@ param (
     [String] $ChocoPackageName = $env:CHOCO_PACKAGE_NAME
 )
 
-# vars
+# Vars
 $ErrorActionPreference = "Stop"
 $nuspecFilename = "$($ChocoPackageName).nuspec"
 
-# get version
+# Get version from nuspec metadata file
 [xml]$spec = Get-Content $nuspecFilename
 $version = $spec.package.metadata.version
 
 Write-Output "STARTING tests..."
 
+# Installation test
 Write-Output "`nTESTING: Installation of package should work..."
 choco install -y --no-progress $ChocoPackageName -source .
 if ((-not $?) -and $ErrorActionPreference -eq "Stop") { exit $LastExitCode } # external command error check
 
+# Version test
 Write-Output "`nTESTING: Package version output..."
 $versionOutput = velero version --client-only | Out-String
 if ((-not $?) -and $ErrorActionPreference -eq "Stop") { exit $LastExitCode } # external command error check
 
-if ($versionOutput -notmatch $version) {
+if ($versionOutput -match $version) {
+    Write-Output "Package version output contained expected version [$version]"
+} else {
     throw "ERROR: Package version output doesn't contain expected version [$version]"
 }
 
+# Uninstallation test
 Write-Output "`nTESTING: Uninstallation of package should work..."
 choco uninstall -y $ChocoPackageName -source .
 if ((-not $?) -and $ErrorActionPreference -eq "Stop") { exit $LastExitCode } # external command error check
