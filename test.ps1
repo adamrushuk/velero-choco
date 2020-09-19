a82ba4bd55e7cfd596f044df984a81f37a93ee5c
Adam Rush <adamrushuk@gmail.com>
2020-09-19 12:08:41 +0100

    Fixed ErrorActionPreference

--- test.ps1
+++ test.ps1
@@ -13,14 +13,14 @@ Write-Output "STARTING tests..."
 
 Write-Output "TESTING: Installation of package should work..."
 choco install -y $chocoPackageName -source . -version $version
-if ((-not $?) -and $ErrorAction -eq "Stop") { exit $LastExitCode } # external command error check
+if ((-not $?) -and $ErrorActionPreference -eq "Stop") { exit $LastExitCode } # external command error check
 
 Write-Output "TESTING: Package version output..."
 velero version --client-only
-if ((-not $?) -and $ErrorAction -eq "Stop") { exit $LastExitCode } # external command error check
+if ((-not $?) -and $ErrorActionPreference -eq "Stop") { exit $LastExitCode } # external command error check
 
 Write-Output "TESTING: Uninstallation of package should work..."
 choco uninstall -y $chocoPackageName -source .
-if ((-not $?) -and $ErrorAction -eq "Stop") { exit $LastExitCode } # external command error check
+if ((-not $?) -and $ErrorActionPreference -eq "Stop") { exit $LastExitCode } # external command error check
 
 Write-Output "FINISHED: tests."
