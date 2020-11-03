75740386326f78e0bd455ab83f5d7027a3e9bda6
Adam Rush <adamrushuk@gmail.com>
2020-11-03 16:09:54 +0000

    Bumped to v1.5.2

--- velero/tools/chocolateyinstall.ps1
+++ velero/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $ErrorActionPreference = 'Stop'
 $packageName = 'velero'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
-  checksum64     = 'e1fae1cb5d704ed1e2411f42e1ec60e7cd6bf55b0c2588836b3d29572c9e636d'
+  checksum64     = '92003b547c5ff289cf0a6ea1b13dceb638a3c2fb64632dedbfbd0d0a6fee51ba'
   checksumType64 = 'sha256'
   packageName    = $packageName
   unzipLocation  = $toolsDir
-  url64bit       = 'https://github.com/vmware-tanzu/velero/releases/download/v1.5.1/velero-v1.5.1-windows-amd64.tar.gz'
+  url64bit       = 'https://github.com/vmware-tanzu/velero/releases/download/v1.5.2/velero-v1.5.2-windows-amd64.tar.gz'
 }
 
 # Cleanup previous installs
