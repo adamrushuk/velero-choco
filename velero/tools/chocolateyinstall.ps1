4c18782f4d0af1fc93dbf16f9e2a767e9959601c
adamrushuk <11696118+adamrushuk@users.noreply.github.com>
2025-07-31 09:54:12 +0000

    Updated Velero to 1.16.2

--- velero/tools/chocolateyinstall.ps1
+++ velero/tools/chocolateyinstall.ps1
@@ -5,10 +5,10 @@ $ErrorActionPreference = 'Stop'
 
 # vars
 $packageName = 'velero'
-$version = '1.16.1'
+$version = '1.16.2'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
-  checksum64     = '114a1b377fd38e284ffb297379b7dfe023c96a4508a1d9d3447c50079c5b0e81'
+  checksum64     = 'fe144df2e47925021f45f5448c9873345f62bc777d263d20d1d7f7f09d749a47'
   checksumType64 = 'sha256'
   packageName    = $packageName
   unzipLocation  = $toolsDir
@@ -24,3 +24,4 @@ Install-ChocolateyZipPackage @packageArgs
 # Untar binary
 $file = Get-ChildItem -File -Path $env:ChocolateyInstall\lib\$packageName\tools\ -Filter *.tar
 Get-ChocolateyUnzip -fileFullPath $file.FullName -destination $env:ChocolateyInstall\lib\$packageName\tools\
+
